c37fd66239bb711d76265664cd4200776856cded
Ellen Zheng <hinotan@gmail.com>
2014-04-10 01:03:52 +1200

    Fix char in meta title.

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
 <head>
 	<meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Ellen & Shin – We’re getting married!</title>
+	<title>Ellen & Shin - We're getting married!</title>
   <meta name="designer" content="UPRISE" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
 
